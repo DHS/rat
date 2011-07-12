44927ee3af79835a900c7248eeb6d5596004a422
David Haywood Smith <davehs@gmail.com>
2011-07-12 08:45:40 +0100

    Added upload to new item form

--- themes/default/items_new.php
+++ themes/default/items_new.php
@@ -3,11 +3,9 @@
 
 if (!empty($_SESSION['user'])) { ?>
 
-<div class="center_container">
-
 <h2>New <?php echo $app['items']['name']; ?></h2>
 
-<form action="item.php" method="post">
+<form action="item.php" method="post" enctype="multipart/form-data">
 
 	<table class="center">
 		<?php
@@ -15,12 +13,14 @@ if (!empty($_SESSION['user'])) { ?>
 			echo '<tr><td class="align_right">'.$GLOBALS['app']['items']['titles']['name'].': </td><td class="alight_left"><input type="text" name="title" size="50" value="'.$_GET['title'].'" /></td></tr>';
 		?>
 		<tr><td class="align_right"><?php echo $GLOBALS['app']['items']['content']['name']; ?>: </td><td class="align_left"><textarea name="content" rows="5" cols="50"><?php echo $_GET['content']; ?></textarea></td></tr>
+		<?php
+		if ($GLOBALS['app']['items']['uploads']['enabled'] == TRUE)
+			echo '<tr><td class="align_right"><label for="upload">'.$GLOBALS['app']['items']['uploads']['name'].':</label></td><td class="alight_left"><input type="file" name="upload" id="upload" /></td></tr>';
+		?>
 		<tr><td></td><td class="align_left"><input type="submit" value="Submit" /></td></tr>
 	</table>
 	
 </form>
-
-</div>
 
 <p>&nbsp;</p>
 
