d5987357b8a20b54a8b6ff38abf3c66b06b4085d
David Haywood Smith <davehs@gmail.com>
2011-07-14 23:57:53 +0100

    Tightened up user profile template

--- themes/default/user_profile.php
+++ themes/default/user_profile.php
@@ -1,35 +1,20 @@
 
-<?php
-
-// Masive if, only show profile if there is some data
-if ($user['full_name'] != NULL || $user['bio'] != NULL || $user['url'] != NULL) {
-
-?>
+<?php if ($user['full_name'] != NULL || $user['bio'] != NULL || $user['url'] != NULL) { // Only show profile if there is some data ?>
 
 <div class="center_container">
 <table class="center">
 
-<?php
-
-if ($user['full_name'] != NULL)
-echo '<tr><td class="align_right"><strong>Name</strong></td><td class="align_left" style="padding-left: 10px;">'.$user['full_name'].'</td></tr>';
-	
-if ($user['bio'] != NULL)
-echo '<tr><td class="align_right"><strong>Bio</strong></td><td class="align_left" style="padding-left: 10px;">'.$user['bio'].'</td></tr>';
-
-if ($user['url'] != NULL)
-echo '<tr><td class="align_right"><strong>URL</strong></td><td class="align_left" style="padding-left: 10px;"><a href="'.$user['url'].'" target="_new">'.$user['url'].'</a></td></tr>';
-
-?>
+<?php if ($user['full_name'] != NULL) { ?>
+<tr><td class="align_right"><strong>Name</strong></td><td class="align_left" style="padding-left: 10px;"><?php echo $user['full_name']; ?></td></tr>
+<?php } if ($user['bio'] != NULL) { ?>
+<tr><td class="align_right"><strong>Bio</strong></td><td class="align_left" style="padding-left: 10px;"><?php echo $user['bio']; ?></td></tr>
+<?php } if ($user['url'] != NULL) { ?>
+<tr><td class="align_right"><strong>URL</strong></td><td class="align_left" style="padding-left: 10px;"><a href="<?php echo $user['url']; ?>" target="_new"><?php echo $user['url']; ?></a></td></tr>
+<?php } ?>
 
 </table>
 </div>
 
 <p />
 
-<?php
-
-}
-// end massive if
-
-?>
+<?php } ?>
