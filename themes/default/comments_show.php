84384652f035bd3c3aed2ce93c53df5aa6e3bdf5
David Haywood Smith <davehs@gmail.com>
2011-07-17 09:14:06 +0100

    Revert "Making a start on liking comments"
    This reverts commit 42d89184fb275c1e819a3ddeca90bde39d38abd9.

--- themes/default/comments_show.php
+++ themes/default/comments_show.php
@@ -1,29 +1,29 @@
-<span id="comments_<?php echo $item['id']; ?>">
-
 <?php
 
 if (count($item['comments']) > 0) {
 
-foreach ($item['comments'] as $comment) {
+	echo '<span id="comments_'.$item['id'].'">';
 
-?>
+	foreach ($item['comments'] as $comment) {
+		
+		echo '
+		<p class="meta">
+		"'.$comment['content'].'" - <a href="user.php?id='.$comment['user']['id'].'">'.$comment['user']['username'].'</a>';
+		if ($comment['user']['id'] == $_SESSION['user']['id']) {
+			echo ' &middot; <span style="font-size: 50%;"><a href="#" onclick="comment_remove('.$comment['user']['id'].', '.$item['id'].', '.$comment['id'].'); return false;">Delete</a></span>';
+		}
+		echo '
+		</p>';
+	
+	}
+	
+	echo '</span>';
 
-    <p class="meta">
-      "<?php echo $comment['content']; ?>" - <a href="user.php?id=<?php echo $comment['user']['id']; ?>"><?php echo $comment['user']['username']; ?></a>
-<?php
-if (!empty($_SESSION['user']['id']) && $GLOBALS['app']['items']['comments']['upvotes'] == TRUE)
-	echo ' &middot; <span class="small"><a href="#" onclick="alert('.$comment['user']['id'].', '.$item['id'].', '.$comment['id'].'); return false;">Like</a></a></span>';
-if ($comment['user']['id'] == $_SESSION['user']['id'])
-	echo ' &middot; <span class="small"><a href="#" onclick="comment_remove('.$comment['user']['id'].', '.$item['id'].', '.$comment['id'].'); return false;">Delete</a></span>';
-?>
-    </p>
+} else {
+	// no comments yet but show empty div fo das ajax
 
-<?php
+	echo '<span id="comments_'.$item['id'].'"></span>';
 
 }
 
-}
-
 ?>
-
-</span>
