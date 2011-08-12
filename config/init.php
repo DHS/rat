c3c88a2af1ba62a0cf88d4e8552092441a999b8e
David Haywood Smith <davehs@gmail.com>
2011-08-12 15:31:13 +0100

    Closes #3: Autoload models

--- config/init.php
+++ config/init.php
@@ -3,26 +3,42 @@
 // Config file contains lots of handy variables
 require_once 'config/config.php';
 
-// Convert config vars to object, create app object, add config vars to app object
+// Save config vars for later
 $app_vars = $app;
 unset($app);
-class app {
-}
-$app = new app;
-$app = (object) $app_vars;
 
-require_once 'models/item.php';
-$app->item = new item;
+// Define app class
+class app {
+	
+	function __construct() {
+
+		// Load models
+		$handle = opendir('models');
+		while (false !== ($file = readdir($handle))) {
+			$model = substr($file, 0, -4);
+			if ($file[0] != '.') {
+				include "models/$model.php";
+				$this->$model = new $model;
+			}
+		}
+
+	}
+	
+}
+
+// Create new instance of app
+$app = new app;
+
+// Add app vars back in
+foreach ($app_vars as $key => $value) {
+	$app->$key = $value;
+}
 
 // Setup database
 require_once 'config/database.php';
 
 // Start session
 session_start();
-
-// Load admin model
-require_once 'models/admin.php';
-$app->admin = new admin;
 
 // Finds page name
 preg_match("/[a-zA-Z0-9]+\.php/", $_SERVER['PHP_SELF'], $result);
@@ -50,21 +66,5 @@ if ($_SESSION['user'] == NULL && $app->private == TRUE && in_array($result[0], $
 	}
 
 }
-
-// Load other models
-require_once 'models/user.php';
-$app->user = new user;
-require_once 'models/invite.php';
-$app->invite = new invite;
-require_once 'models/friend.php';
-$app->friend = new friend;
-require_once 'models/item.php';
-$app->item = new item;
-require_once 'models/comment.php';
-$app->comment = new comment;
-require_once 'models/like.php';
-$app->like = new like;
-require_once 'models/search.php';
-$app->search = new search;
 
 ?>
