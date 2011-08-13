6658fe99a172251b1e0776ab5201fb63062dbb5f
David Haywood Smith <davehs@gmail.com>
2011-08-14 00:45:58 +0100

    Moved controllers to their own directory

---
+++ controllers/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'config/init.php';
+
+// Header
+
+$app->page->name = $app->config->tagline;
+$app->loadView('header');
+
+// Intro paragraph
+
+//$app->loadView('index');
+
+// New item form
+
+$app->loadView('items_add');
+
+// If friends is enabled, show feed of their activity
+
+if ($app->config->friends['enabled'] == TRUE) {
+
+	if (empty($_SESSION['user'])) {
+		
+		echo '<p>Please <a href="login.php">login</a>.</p>';
+		
+	} else {
+		
+		// Show feed of friends' activity
+		
+		$app->page->items = $app->item->list_feed();
+		$app->loadView('items_list');
+		
+	}
+
+} else {
+	
+	// Friends not enabled so don't show recent items
+
+	$app->page->items = $app->item->list_all();
+	$app->loadView('items_list');	
+	
+}
+
+// Footer
+
+$app->loadView('footer');
+
+?>
