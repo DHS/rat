23672dec302a81bfae1ff31f92b03aac8e530936
David Haywood Smith <davehs@gmail.com>
2011-08-17 18:53:21 +0100

    Tidying up home controller and views

--- controllers/home.php
+++ controllers/home.php
@@ -2,12 +2,38 @@
 
 class home {
 	
-	function index($id = NULL) {
+	// Show stream of everyone's activity
+	function index() {
 		
 		global $app;
 		
+		$app->page->name = $app->config->tagline;
 		$app->page->items = $app->item->list_all();
+		
 		$app->loadLayout('items_list');
+		
+	}
+	
+	// Show feed of friends' activity
+	function feed() {
+		
+		global $app;
+		
+		if ($app->config->friends['enabled'] == TRUE) {
+			
+			// If friends enabled then show feed of friends' activity
+			
+			$app->page->name = $app->config->tagline;
+			$app->page->items = $app->item->list_feed($_SESSION['user']['id']);
+			$app->loadLayout('items_list');
+			
+		} else {
+			
+			// Friends not enabled so fall back to showing everyone's activity
+			
+			$this->index();
+			
+		}
 		
 	}
 	
