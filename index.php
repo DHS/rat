8c325b1ff83d28c465b61538670fe54dfa8ea734
David Haywood Smith <davehs@gmail.com>
2011-08-17 15:51:11 +0100

    More pathetic housework

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 session_start();
 
 require_once 'lib/application.php';
-require_once 'config/config.php';
 
 // Load config early to get base_dev_dir and default_controller
+require_once 'config/config.php';
 $config = new config;
 
 // Get request from server, split into segments, store as controller, view, id and params
@@ -13,13 +13,21 @@ $request = substr($_SERVER['REQUEST_URI'], strlen($config->dev_base_dir));
 
 // Split at '.' and before '?' to obtain request format
 $segments = preg_split("/\./", $request);
-$format = preg_split("/\?/", $segments[1]); $format = $format[0];
+$format = preg_split("/\?/", $segments[1]);
+$format = $format[0];
 
 // Split request at each '/' to obtain route
 $segments = preg_split("/\//", $segments[0]);
 
-$uri = array('controller' => $segments[1], 'view' => $segments[2], 'id' => $segments[3], 'format' => $format, 'params' => $_GET);
-if (empty($uri['controller'])) { $uri['controller'] = $config->default_controller; };
+$uri = array(	'controller'	=> $segments[1],
+				'view'			=> $segments[2],
+				'id'			=> $segments[3],
+				'format'		=> $format,
+				'params'		=> $_GET
+			);
+
+if (empty($uri['controller']))
+	$uri['controller'] = $config->default_controller;
 
 // Instantiate a new application
 $app = new application($uri);
