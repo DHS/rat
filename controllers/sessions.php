810bda60483afeaf2e368eb45e04e667be8689c8
David Haywood Smith <davehs@gmail.com>
2011-08-19 12:00:42 +0100

    Created sessions controller

---
+++ controllers/sessions.php
@@ -0,0 +1,31 @@
+<?php
+
+class SessionsController {
+	
+	function index() {
+		
+		global $app;
+		
+		// Not needed?
+		
+	}
+	
+	function add() {
+		
+		global $app;
+		
+		$app->loadLayout('sessions/add');
+		
+	}
+	
+	function remove() {
+		
+		global $app;
+		
+		
+		
+	}
+	
+}
+
+?>
