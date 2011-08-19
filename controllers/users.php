0a3d84e288caf1e9bf9a851734fa21327d604c9e
David Haywood Smith <davehs@gmail.com>
2011-08-19 11:42:06 +0100

    User update

--- controllers/users.php
+++ controllers/users.php
@@ -7,7 +7,7 @@ class UsersController {
 		
 		global $app;
 		
-		
+		// Not needed?
 		
 	}
 	
@@ -32,78 +32,13 @@ class UsersController {
 		
 	}
 	
-	function json($username) {
+	function update($id) {
 		
 		global $app;
 		
-		$user['user'] = $app->user->get_by_username($username);
-		$user['items'] = $app->item->list_user($user['user']['id']);
-		$app->page->json = $user;
-		$app->loadView('json');
+		$app->page->user = $app->user->get($id);
 		
-	}
-	
-}
-
-/*
-class users {
-	
-	function show($username, $page = 1) {
-		
-		global $app;
-		
-		$limit = 2;
-		$offset = ($page - 1) * $limit;
-		
-		$app->page->user = $app->user->get_by_username($username);
-		$app->page->items = $app->item->list_user($app->page->user['id'], $limit, $offset);
-		
-		// Header
-
-		if (isset($app->plugins->gravatar))
-			$app->page->title_gravatar = $app->page->user['email'];
-
-		$app->page->head_title = $app->page->user['name'].' on '.$app->config->name;
-		$app->page->title = '<a href="/'.$app->page->user['username'].'">'.$app->page->user['name'].'</a> on <a href="/">'.$app->config->name.'</a>';
-
-		$app->loadView('header');
-
-		// Show profile
-
-		$app->loadView('users/profile');
-
-		// Show follow button
-
-		if ($app->config->friends['enabled'] == TRUE)
-			$app->loadView('friends_button');
-
-		// Show number of points
-
-		if (isset($app->plugins->points))
-			$app->loadView('points');
-
-		// Show new item form
-
-		//if ($_SESSION['user']['post_permission'] == 1)
-		//	$app->loadView('items_add');
-
-		// List all items for this user
-
-		if (count($app->page->items) > 0) {
-
-			$app->loadView('items_list_user');
-
-		} else {
-
-			// If own page and no post_permission OR someone else's page show 'no articles yet'
-			if (($_SESSION['user']['id'] == $app->page->user['id'] && $_SESSION['user']['post_permission'] == 0) || $_SESSION['user']['id'] != $app->page->user['id'])
-				echo '<p>'.$app->page->user['username'].' hasn\'t published any '.$app->config->items['name_plural'].' yet.</p>';
-
-		}
-
-		// Footer
-
-		$app->loadView('footer');
+		$app->loadLayout('users/update');
 		
 	}
 	
@@ -119,5 +54,5 @@ class users {
 	}
 	
 }
-*/
+
 ?>
--- themes/default/users/update.php
+++ themes/default/users/update.php
@@ -1,4 +1,20 @@
 
+<h2>Change password</h2>
+
+<form action="/settings/password" method="post">
+
+	<table>
+		<tr><td class="align_right">Old password:</td><td><input type="password" name="old_password" /></td></tr>
+		<tr><td class="align_right">New password:</td><td><input type="password" name="new_password1" /></td></tr>
+		<tr><td class="align_right">New password again:</td><td><input type="password" name="new_password2" /></td></tr>
+		<tr><td></td><td class="align_left"><input type="submit" value="Save" /></td></tr>
+	</table>
+
+</form>
+
+<p>&nbsp;</p>
+
+
 <h2>Update profile</h2>
 
 <form action="/settings/profile" method="post">
