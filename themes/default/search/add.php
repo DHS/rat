6476873ee23b7c02e45a91a1e96240f98d78d7af
David Haywood Smith <davehs@gmail.com>
2011-08-20 03:09:58 +0100

    Fix search url

--- themes/default/search/add.php
+++ themes/default/search/add.php
@@ -1,6 +1,6 @@
 
 <h2>Search <?php echo $app->config->items['name_plural']; ?></h2>
 
-<form action="/search" method="get">
+<form action="/search/" method="get">
 	<input type="text" name="q" id="q" size="50" value="<?php if (isset($_GET['q'])) echo $_GET['q']; ?>" /> <input type="submit" value="Search" />
 </form>
