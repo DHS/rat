9a1ceb52221016459b45961cf144e972816fdd9c
David Haywood Smith <davehs@gmail.com>
2011-08-23 00:09:05 +0100

    More work on grid layout w/comment box fix, tidier likes

--- themes/default/comments/index.php
+++ themes/default/comments/index.php
@@ -9,15 +9,13 @@ if (count($item['comments']) > 0) {
 
 	foreach ($item['comments'] as $comment) {
 		
-		echo '
-		<p class="meta">
+		echo '<p class="meta">
 		"'.$comment['content'].'" - '.$this->link_to($comment['user']['username'], 'users', 'show', $comment['user']['id']);
 		if ($comment['user']['id'] == $_SESSION['user']['id']) {
 			$url = $this->link_to(NULL, 'comments', 'remove', $comment['id']);
 			echo ' &middot; <span style="font-size: 50%;"><a href="#" onclick="comment_remove('.$item['id'].', \''.$url.'\'); return false;">Delete</a></span>';
 		}
-		echo '
-		</p>';
+		echo '</p>';
 	
 	}
 	
@@ -30,24 +28,18 @@ if (count($item['comments']) > 0) {
 
 }
 
-if ($app->page->show_comment_form == TRUE) {
+if (!isset($item))
+	$item = $app->page->item;
 
-	if (!isset($item))
-		$item = $app->page->item;
-	
-	if ($app->config->items['comments']['enabled'] == TRUE && ($app->config->private == TRUE || $_SESSION['user'] != NULL)) {
-	
-		$url = $this->link_to(NULL, 'comments', 'add');
-	
-		?>
-	
-		<form action="javascript:comment_add(<?php echo $item['id']; ?>, '<?php echo $url; ?>');" id="comment_form_<?php echo $item['id']; ?>" class="meta" style="margin: 0px; <?php if ($app->page->show_comment_form != TRUE) { echo 'visibility: hidden; height: 0px;'; }?>" method="post">
-			<input type="text" name="content" size="30" value="" /> <input type="submit" value="<?php echo $app->config->items['comments']['name']; ?>" />
-		</form>
-	
-	<?php
-	
-	}
+if ($app->config->items['comments']['enabled'] == TRUE && ($app->config->private == TRUE || $_SESSION['user'] != NULL)) {
+
+	?>
+
+	<form action="javascript:comment_add(<?php echo $item['id']; ?>, '<?php echo $this->link_to(NULL, 'comments', 'add'); ?>');" id="comment_form_<?php echo $item['id']; ?>" class="meta" style="margin: 0px; <?php if ($app->page->show_comment_form != TRUE) { echo 'visibility: hidden; height: 0px;'; }?>" method="post">
+		<input type="text" name="content" size="30" value="" /> <input type="submit" value="<?php echo $app->config->items['comments']['name']; ?>" class="btn" />
+	</form>
+
+<?php
 
 }
 
--- themes/default/items/index.php
+++ themes/default/items/index.php
@@ -5,10 +5,8 @@
 	$app->page->item = $item; ?>
 
   <div class="row">
-    <div class="span2 columns offset4">
-      <?php echo $app->plugins->gravatar->show($item['user']['email'], array('size' => 48)); ?>
-    </div>
-    <div class="span6 columns">
+    <div class="span8 columns offset4">
+      <?php echo $app->plugins->gravatar->show($item['user']['email'], array('size' => 48, 'style' => 'float: left; padding: 0px 10px 10px 0px;')); ?>
       <?php if ($app->config->items['titles']['enabled'] == TRUE && $item['title'] != NULL) { ?>
       <p><?php echo $this->link_to($item['title'], 'items', 'show', $item['id']); ?> by <?php echo $this->link_to($item['user']['username'], 'users', 'show', $item['user']['id']); ?></p>
       <p><?php echo $item['content']; ?></p>
@@ -18,41 +16,38 @@
     </div>
   </div>
   <div class="row">
-    <div class="span8 columns offset4">
+    <div class="span8 columns offset4" style="padding: 5px 0px;">
       <?php $app->loadView('items/meta'); ?>
     </div>
   </div>
-  <?php if ($app->config->items['likes']['enabled'] == TRUE) ?>
+  <?php if ($app->config->items['likes']['enabled'] == TRUE) { ?>
   <div class="row">
-    <div class="span8 columns offset4">
+    <div class="span8 columns offset4" style="padding: 4px 0px;">
       <?php $app->loadView('likes/index'); ?>
     </div>
   </div>
   <?php } ?>
+  <?php if ($app->config->items['comments']['enabled'] == TRUE) { 
+	if (count($item['comments']) > 0) {
+		$app->page->show_comment_form = TRUE;
+	} else {
+		$app->page->show_comment_form = FALSE;
+	}
+  ?>
   <div class="row">
     <div class="span8 columns offset4">
-		<?php if ($app->config->items['comments']['enabled'] == TRUE) {
-			if (count($item['comments']) > 0) {
-				$app->page->show_comment_form = TRUE;
-			} else {
-				$app->page->show_comment_form = FALSE;
-			}
-			$app->loadView('comments/index');
-		} ?>
+    <?php echo $app->loadView('comments/index'); ?>
     </div>
   </div>
+  <?php } ?>
   <div class="row">
     <div class="span16 columns">
-      &nbsp;
-    </div>
-  </div>
-  <div class="row">
-    <div class="span16 columns">
-      &nbsp;
+      <p>&nbsp;</p>
     </div>
   </div>
 
-<?php unset($app->page->item);
-} // end foreach loop ?>
-
-<?php } // end if is_array ?>
+<?php
+unset($app->page->item);
+} // end foreach loop
+} // end if is_array
+?>
