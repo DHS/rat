69b6257e6c47c16340090dc52ac76b59948254c3
David Haywood Smith <davehs@gmail.com>
2011-08-23 00:52:17 +0100

    Gridding getting better

--- themes/default/items/index.php
+++ themes/default/items/index.php
@@ -1,50 +1,69 @@
-
-<?php if (is_array($app->page->items)) { ?>
-
-<?php foreach ($app->page->items as $item) {
-	$app->page->item = $item; ?>
-
-  <div class="row">
-    <div class="span8 columns offset4">
-      <?php echo $app->plugins->gravatar->show($item['user']['email'], array('size' => 48, 'style' => 'float: left; padding: 0px 10px 10px 0px;')); ?>
-      <?php if ($app->config->items['titles']['enabled'] == TRUE && $item['title'] != NULL) { ?>
-      <p><?php echo $this->link_to($item['title'], 'items', 'show', $item['id']); ?> by <?php echo $this->link_to($item['user']['username'], 'users', 'show', $item['user']['id']); ?></p>
-      <p><?php echo $item['content']; ?></p>
-      <?php } else { ?>
-      <p><?php echo $this->link_to($item['user']['username'], 'users', 'show', $item['user']['id']); ?> <?php echo $item['content']; ?></p>
-      <?php } ?>
-    </div>
-  </div>
-  <div class="row">
-    <div class="span8 columns offset4" style="padding: 5px 0px;">
-      <?php $app->loadView('items/meta'); ?>
-    </div>
-  </div>
-  <?php if ($app->config->items['likes']['enabled'] == TRUE) { ?>
-  <div class="row">
-    <div class="span8 columns offset4" style="padding: 4px 0px;">
-      <?php $app->loadView('likes/index'); ?>
-    </div>
-  </div>
-  <?php } ?>
-  <?php if ($app->config->items['comments']['enabled'] == TRUE) { 
+<?php if (is_array($app->page->items)) {
+foreach ($app->page->items as $item) {
+	
+	// Prepare vars for comment and like views to be loaded in due course
+	$app->page->item = $item;
+	
+	// Populate some vars
+	if ($app->config->items['titles']['enabled'] == TRUE && $item['title'] != NULL) {
+		$content = '<h4>'.$this->link_to($item['title'], 'items', 'show', $item['id']).' <small>by '.$this->link_to($item['user']['username'], 'users', 'show', $item['user']['id']).'</small></h4>';
+		$content .= '<p>'.$item['content'].'</p>';
+	} else {
+		$content = '<p>'.$this->link_to($item['user']['username'], 'users', 'show', $item['user']['id']).' '.$item['content'].'</p>';
+	}
+	
+	// Comment form toggle
 	if (count($item['comments']) > 0) {
 		$app->page->show_comment_form = TRUE;
 	} else {
 		$app->page->show_comment_form = FALSE;
 	}
-  ?>
+	
+?>
+
+  <!-- Begin item -->
+
+  <!-- Content -->
+  <div class="row">
+    <div class="span8 columns offset4">
+      <?php echo $app->plugins->gravatar->show($item['user']['email'], array('size' => 48, 'style' => 'float: left; padding: 0px 10px 10px 0px;')); ?>
+      <?php echo $content; ?>
+    </div>
+  </div>
+
+  <!-- Meta -->
+  <div class="row">
+    <div class="span8 columns offset4" style="padding: 5px 0px;">
+      <?php echo $app->loadView('items/meta'); ?>
+    </div>
+  </div>
+
+<?php if ($app->config->items['likes']['enabled'] == TRUE) { ?>
+  <!-- Likes -->
+  <div class="row">
+    <div class="span8 columns offset4" style="padding: 4px 0px;">
+      <?php $app->loadView('likes/index'); ?>
+    </div>
+  </div>
+<?php } ?>
+
+<?php if ($app->config->items['comments']['enabled'] == TRUE) { ?>
+  <!-- Comments -->
   <div class="row">
     <div class="span8 columns offset4">
     <?php echo $app->loadView('comments/index'); ?>
     </div>
   </div>
-  <?php } ?>
+<?php } ?>
+
+  <!-- Spacer -->
   <div class="row">
     <div class="span16 columns">
       <p>&nbsp;</p>
     </div>
   </div>
+
+  <!-- End item -->
 
 <?php
 unset($app->page->item);
