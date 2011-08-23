e514eb7b245fc86aedb23b8f98088270d1868040
David Haywood Smith <davehs@gmail.com>
2011-08-23 01:16:44 +0100

    Fixed disabling titles & content, closes #1

--- themes/default/items/add.php
+++ themes/default/items/add.php
@@ -9,11 +9,16 @@ if (!empty($_SESSION['user'])) { ?>
 
 	<table>
 		<?php
-		if ($app->config->items['titles']['enabled'] == TRUE)
-			echo '<tr><td class="align_right">'.$app->config->items['titles']['name'].': </td><td class="alight_left"><input type="text" name="title" size="50" value="'; if (isset($_GET['title'])) echo $_GET['title']; echo '" /></td></tr>';
-		?>
-		<tr><td class="align_right"><?php echo $app->config->items['content']['name']; ?>: </td><td class="align_left"><textarea name="content" rows="5" cols="50"><?php if (isset($_GET['content'])) echo $_GET['content']; ?></textarea></td></tr>
-		<?php
+		if ($app->config->items['titles']['enabled'] == TRUE) {
+			echo '<tr><td class="align_right">'.$app->config->items['titles']['name'].': </td><td class="alight_left"><input type="text" name="title" size="50" value="';
+			if (isset($_GET['title'])) echo $_GET['title'];
+			echo '" /></td></tr>';
+		}
+		if ($app->config->items['content']['enabled'] == TRUE) {
+			echo '<tr><td class="align_right">'.$app->config->items['content']['name'].': </td><td class="align_left"><textarea name="content" rows="5" cols="50">';
+			if (isset($_GET['content'])) echo $_GET['content'];
+			echo '</textarea></td></tr>';
+		}
 		if ($app->config->items['uploads']['enabled'] == TRUE)
 			echo '<tr><td class="align_right"><label for="file">'.$app->config->items['uploads']['name'].':</label></td><td class="alight_left"><input type="file" name="file" id="file" /></td></tr>';
 		?>
