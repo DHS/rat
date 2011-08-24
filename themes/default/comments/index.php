9ba0ca00812c87899c0d5b5bb64d4801e44628e8
David Haywood Smith <davehs@gmail.com>
2011-08-25 00:55:27 +0100

    Some more objecty fixes to make things work

--- themes/default/comments/index.php
+++ themes/default/comments/index.php
@@ -10,7 +10,7 @@ if (count($this->item->comments) > 0) {
 		"'.$comment->content.'" - '.$this->link_to($comment->user->username, 'users', 'show', $comment->user->id);
 		if ($comment->user->id == $_SESSION['user']['id']) {
 			$url = $this->link_to(NULL, 'comments', 'remove', $comment->id);
-			echo ' &middot; <span style="font-size: 50%;"><a href="#" onclick="comment_remove('.$item->id.', \''.$url.'\'); return false;">Delete</a></span>';
+			echo ' &middot; <span class="small"><a href="#" onclick="comment_remove('.$this->item->id.', \''.$url.'\'); return false;">Delete</a></span>';
 		}
 		echo '</p>';
 	
--- themes/default/items/show.php
+++ themes/default/items/show.php
@@ -1,23 +1,21 @@
 
-<?php $item = $this->item; ?>
-
 <div class="row">
   <div class="span8 columns offset4">
 
 <?php if ($this->config->items['titles']['enabled'] == TRUE) { ?>
 
-<h2><?php echo $item->title; ?></h2>
+<h2><?php echo $this->item->title; ?></h2>
 
-<?php if ($this->config->items['uploads']['enabled'] == TRUE && $item->image != NULL) { ?>
-	<a href="<?php echo $this->config->items['uploads']['directory']; ?>/originals/<?php echo $item->image; ?>"><img src="<?php echo $this->config->items['uploads']['directory']; ?>/stream/<?php echo $item->image; ?>" /></a>
+<?php if ($this->config->items['uploads']['enabled'] == TRUE && $this->item->image != NULL) { ?>
+	<a href="<?php echo $this->config->items['uploads']['directory']; ?>/originals/<?php echo $this->item->image; ?>"><img src="<?php echo $this->config->items['uploads']['directory']; ?>/stream/<?php echo $this->item->image; ?>" /></a>
 
 <?php } ?>
 
-<p><?php echo $item->content; ?></p>
+<p><?php echo $this->item->content; ?></p>
 
 <?php } else { ?>
 
-<p><?php echo $item->content; ?></p>
+<p><?php echo $this->item->content; ?></p>
 
 <?php } ?>
 
@@ -25,12 +23,12 @@
 
 <?php
 if (isset($this->plugins->gravatar)) {
-	$gravatar = $this->plugins->gravatar->show($item->user->email, array('size' => 20, 'style' => "margin-top: -5px;"));
-	echo $this->link_to($gravatar, 'users', 'show', $item->user->id).' ';
+	$gravatar = $this->plugins->gravatar->show($this->item->user->email, array('size' => 20, 'style' => "margin-top: -5px;"));
+	echo $this->link_to($gravatar, 'users', 'show', $this->item->user->id).' ';
 }
 ?>
 
-<?php echo $this->link_to($item->user->username, 'users', 'show', $item->user->id); ?>
+<?php echo $this->link_to($this->item->user->username, 'users', 'show', $this->item->user->id); ?>
 
 &middot; <?php $this->loadView('items/meta'); ?>
 
