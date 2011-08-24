a73a8351391854d6eb0ea6b03386b045377ead4f
David Haywood Smith <davehs@gmail.com>
2011-08-25 00:35:25 +0100

    Fixed new item form

--- themes/default/items/add.php
+++ themes/default/items/add.php
@@ -3,14 +3,14 @@
 
 if (!empty($_SESSION['user'])) { ?>
 
-      <form <?php echo $this->link_to(NULL, 'items', 'add'); ?> method="post" enctype="multipart/form-data">
+      <form action="<?php echo $this->link_to(NULL, 'items', 'add'); ?>" method="post" enctype="multipart/form-data">
         <fieldset>
           <legend>New <?php echo $this->config->items['name']; ?></legend>
 <?php if ($this->config->items['titles']['enabled'] == TRUE) { ?>
             <div class="clearfix">
               <label for="title"><?php echo $this->config->items['titles']['name']; ?></label>
               <div class="input">
-                <input class="medium" name="title" size="50" type="text" value="<?php if (isset($_GET['title'])) echo $_GET['title']; ?>" />
+                <input class="medium" name="title" size="50" type="text" value="<?php if (isset($_GET['title'])) { echo $_GET['title']; } ?>" />
               </div>
             </div> <!-- /clearfix -->
 <?php } ?>
@@ -18,7 +18,7 @@ if (!empty($_SESSION['user'])) { ?>
             <div class="clearfix">
               <label for="content"><?php echo $this->config->items['content']['name']; ?></label>
               <div class="input">
-                <textarea name="content" rows="5" cols="50"><?php if (isset($_GET['content'])) echo $_GET['content']; ?></textarea>
+                <textarea name="content" rows="5" cols="50"><?php if (isset($_GET['content'])) { echo $_GET['content']; } ?></textarea>
               </div>
             </div> <!-- /clearfix -->
 <?php } ?>
