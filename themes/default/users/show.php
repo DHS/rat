58241872274798b3b7b109c1a6fd3eb2ec62c99e
David Haywood Smith <davehs@gmail.com>
2011-08-24 22:04:12 +0100

    Converted remaining user refs in view to objects

--- themes/default/users/show.php
+++ themes/default/users/show.php
@@ -12,8 +12,8 @@
 	} else {
 
 		// If own page and no post_permission OR someone else's page show 'no articles yet'
-		if (($_SESSION['user']['id'] == $this->user['id'] && $_SESSION['user']['post_permission'] == 0) || $_SESSION['user']['id'] != $this->user['id'])
-			echo '<p>'.$this->user['username'].' hasn\'t published any '.$this->config->items['name_plural'].' yet.</p>';
+		if (($_SESSION['user']['id'] == $this->user->id && $_SESSION['user']['post_permission'] == 0) || $_SESSION['user']['id'] != $this->user->id)
+			echo '<p>'.$this->user->username.' hasn\'t published any '.$this->config->items['name_plural'].' yet.</p>';
 
 	}
 
@@ -22,16 +22,16 @@
   </div>
   <div class="span4 columns">
 
-<?php if ($this->user['full_name'] != NULL || $this->user['bio'] != NULL || $this->user['url'] != NULL) { // Only show profile if there is some data ?>
+<?php if ($this->user->full_name != NULL || $this->user->bio != NULL || $this->user->url != NULL) { // Only show profile if there is some data ?>
 
 <h3>Profile</h3>
 
-<?php if ($this->user['full_name'] != NULL) { ?>
-      <strong>Name</strong> <?php echo $this->user['full_name']; ?>
-<?php } if ($this->user['bio'] != NULL) { ?>
-      <strong>Bio</strong> <?php echo $this->user['bio']; ?>
-<?php } if ($this->user['url'] != NULL) { ?>
-      <strong>URL</strong> <a href="<?php echo $this->user['url']; ?>" target="_new"><?php echo $this->user['url']; ?></a>
+<?php if ($this->user->full_name != NULL) { ?>
+      <strong>Name</strong> <?php echo $this->user->full_name; ?>
+<?php } if ($this->user->bio != NULL) { ?>
+      <strong>Bio</strong> <?php echo $this->user->bio; ?>
+<?php } if ($this->user->url != NULL) { ?>
+      <strong>URL</strong> <a href="<?php echo $this->user->url; ?>" target="_new"><?php echo $this->user->url; ?></a>
 <?php } ?>
 
 <p />
