925af368a30a2242c16627a5717edf34c0fe9a18
David Haywood Smith <davehs@gmail.com>
2011-08-26 14:49:35 +0100

    Tiny tidy for friend partial

--- themes/default/partials/friend.php
+++ themes/default/partials/friend.php
@@ -4,7 +4,9 @@
   
   <?php if (empty($_SESSION['user'])) { ?>
 
-    <a href="<?php $this->link_to(NULL, 'sessions', 'add'); ?>/?redirect_to=/<?php echo $this->link_to(NULL, 'users', 'show', $this->user->id); ?>" class="btn"><?php if ($this->config->friends['asymmetric'] == TRUE) { echo 'Follow'; } else { echo 'Add friend'; } ?></a>
+    <a href="<?php $this->link_to(NULL, 'sessions', 'add'); ?>/?redirect_to=/<?php echo $this->link_to(NULL, 'users', 'show', $this->user->id); ?>" class="btn">
+    <?php if ($this->config->friends['asymmetric'] == TRUE) { echo 'Follow'; } else { echo 'Add friend'; } ?>
+    </a>
 
   <?php } else { ?>
   
