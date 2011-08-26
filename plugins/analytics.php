a03cc50eea57b767e52786dcb6810e33c1890e8f
David Haywood Smith <davehs@gmail.com>
2011-08-26 15:08:31 +0100

    Updated to latest version of H5BP. Closes #16
    404 included for #19 but doesn't work yet

--- plugins/analytics.php
+++ plugins/analytics.php
@@ -30,13 +30,11 @@ class analytics extends Application {
 		if ($this->analytics_id != NULL && SITE_IDENTIFIER == 'live') {
 		
 			return <<<HTML
-
-<!-- mathiasbynens.be/notes/async-analytics-snippet Change UA-XXXXX-X to be your site's ID -->
 <script>
-  var _gaq=[['_setAccount','{$this->analytics_id}'],['_trackPageview'],['_trackPageLoadTime']];
-  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-  s.parentNode.insertBefore(g,s)}(document,'script'));
+  window._gaq = [['_setAccount','{$this->analytics_id}'],['_trackPageview'],['_trackPageLoadTime']];
+  Modernizr.load({
+    load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+  });
 </script>
 
 HTML;
--- themes/default/partials/header.php
+++ themes/default/partials/header.php
@@ -19,6 +19,7 @@ if (isset($this->title)) {
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
@@ -32,28 +33,30 @@ if (isset($this->title)) {
   <meta name="author" content="">
 
   <!-- Mobile viewport optimized: j.mp/bplateviewport -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
 
   <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 
-  <!-- CSS: implied media="all" -->
+  <!-- CSS: implied media=all -->
   <link rel="stylesheet" href="<?php echo BASE_DIR; ?>/themes/<?php echo $this->config->theme; ?>/css/style.css">
-
   <!-- Include Twitter Bootstrap http://twitter.github.com/bootstrap/ -->
   <link rel="stylesheet" href="<?php echo BASE_DIR; ?>/themes/<?php echo $this->config->theme; ?>/css/bootstrap-1.0.0.min.css">
+  <!-- end CSS-->
 
-  <!-- More ideas for your <head> here: h5bp.com/docs/#head-Tips -->
+  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
 
-  <!-- All JavaScript at the bottom, except for Modernizr and Respond.
+  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
        Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
        For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
-  <script src="<?php echo BASE_DIR; ?>/js/libs/modernizr-2.0.min.js"></script>
+  <script src="<?php echo BASE_DIR; ?>/js/libs/modernizr-2.0.6.min.js"></script>
   <script src="<?php echo BASE_DIR; ?>/js/libs/respond.min.js"></script>
 </head>
 
+
+
 <body>
 
-  <div class="container">
+  <div id="container">
 
 <?php if (isset($_SESSION['user'])) { ?>
 
@@ -128,3 +131,5 @@ if (isset($this->message)) {
 }
 
 ?>
+
+    <div id="main" role="main">
