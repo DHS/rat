628911bb789fcd32765063065360c7caaabd0120
David Haywood Smith <davehs@gmail.com>
2011-08-31 15:10:34 +0100

    share links borked on lists

--- themes/default/partials/item.php
+++ themes/default/partials/item.php
@@ -64,10 +64,10 @@ if (is_array($this->item->likes)) {
 	</p>
 
 <?php if ($this->config->private == FALSE) { ?>
-    <span style="float: right; margin-left: 20px;">
+    <!--<span style="float: right; margin-left: 20px;">
       <a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
       <iframe src="http://www.facebook.com/plugins/like.php?href=<?php echo $this->config->url.substr($_SERVER['REQUEST_URI'], 1); ?>&amp;layout=button_count&amp;show_faces=true&amp;width=100&amp;action=like&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
-    </span>
+    </span>-->
 <?php } ?>
 
 <?php if ($this->config->items['likes']['enabled'] == TRUE) { ?>
