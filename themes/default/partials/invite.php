79000154bdb24f92402b7570063add2912d0aa59
David Haywood Smith <davehs@gmail.com>
2011-09-01 17:16:58 +0100

    fixed uri param refs

--- themes/default/partials/invite.php
+++ themes/default/partials/invite.php
@@ -3,7 +3,7 @@
     <legend>Send an invite</legend>
     <label for="email">Email</label>
     <div class="input">
-      <input class="medium" name="email" size="50" type="text" value="<?php if (isset($this->uri->params['email'])) { echo $this->uri->params['email']; } ?>" />
+      <input class="medium" name="email" size="50" type="text" value="<?php if (isset($this->uri['params']['email'])) { echo $this->uri['params']['email']; } ?>" />
     </div>
     <div class="actions">
       <button type="submit" class="btn">Invite</button>
