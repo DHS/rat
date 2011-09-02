583825e7f3f6c7ddf4d269f93aeca470567c0bc7
David Haywood Smith <davehs@gmail.com>
2011-09-02 14:20:59 +0100

    default routes, closes 57

--- config/routes.php
+++ config/routes.php
@@ -5,13 +5,27 @@ class Routes {
 	// Simple alias
 	public $aliases = array(
 		
-		'/signup' => array('controller' => 'users', 'action' => 'add'),
-		'/login' => array('controller' => 'sessions', 'action' => 'add'),
+		// Static pages
+		
 		'/about' => array('controller' => 'pages', 'action' => 'show', 'id' => 'about'),
 		'/contact' => array('controller' => 'pages', 'action' => 'show', 'id' => 'contact'),
 		'/help' => array('controller' => 'pages', 'action' => 'show', 'id' => 'help'),
+		'/404' => array('controller' => 'pages', 'action' => 'show', 'id' => '404'),
+		
+		// User functions
+		
+		'/signup' => array('controller' => 'users', 'action' => 'add'),
+		'/login' => array('controller' => 'sessions', 'action' => 'add'),
+		'/logout' => array('controller' => 'sessions', 'action' => 'remove'),
+		'/settings' => array('controller' => 'users', 'action' => 'update'),
+		
+		// More complex
+		
+		// eg. /username
 		'/*' => array('controller' => 'users', 'action' => 'show', 'id' => '$1'),
+		// eg. /username/page
 		'/*/*' => array('controller' => 'users', 'action' => 'show', 'id' => '$1', 'page' => '$2'),
+		// eg. /username/item/item_id
 		'/*/item/*' => array('controller' => 'items', 'action' => 'show', 'id' => '$2')
 		
 	);
