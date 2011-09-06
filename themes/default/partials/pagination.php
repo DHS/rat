baf42b1e17946db4a3a9dabded4253f102beb1dd
David Haywood Smith <davehs@gmail.com>
2011-09-06 01:58:28 +0100

    scrappy fix for pagination, closes #24

--- themes/default/partials/pagination.php
+++ themes/default/partials/pagination.php
@@ -1,6 +1,11 @@
 <div class="pagination">
   <ul>
+<?php if ($this->uri['controller'] == 'items') { ?>
+    <li class="prev"><a href="<?php echo $this->url_for($this->uri['controller'], $this->uri['action']); ?>/?page=<?php echo $this->uri['params']['page']-1; ?>">&larr; Previous</a></li>
+    <li class="next"><a href="<?php echo $this->url_for($this->uri['controller'], $this->uri['action']); ?>/?page=<?php echo $this->uri['params']['page']+1; ?>">Next &rarr;</a></li>
+<?php } else { ?>
     <li class="prev"><a href="<?php echo $this->url_for($this->uri['controller'], $this->uri['action'], $this->uri['params']['id']); ?>/?page=<?php echo $this->uri['params']['page']-1; ?>">&larr; Previous</a></li>
     <li class="next"><a href="<?php echo $this->url_for($this->uri['controller'], $this->uri['action'], $this->uri['params']['id']); ?>/?page=<?php echo $this->uri['params']['page']+1; ?>">Next &rarr;</a></li>
+<?php } ?>
   </ul>
 </div>
