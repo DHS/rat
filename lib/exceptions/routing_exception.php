662e761aa4611f75de792fd738ec79f8ee5f1945
David Haywood Smith <davehs@gmail.com>
2011-09-06 02:02:16 +0100

    whitespace

--- lib/exceptions/routing_exception.php
+++ lib/exceptions/routing_exception.php
@@ -1,15 +1,15 @@
 <?php
 
 class RoutingException extends Exception {
-
-	public function __construct($uri, $message, $code = 0, Exception $previous = null) {
-
-		parent::__construct($message, $code, $previous);
-
-		$this->uri = $uri;
 	
+	public function __construct($uri, $message, $code = 0, Exception $previous = null) {
+		
+		parent::__construct($message, $code, $previous);
+		
+		$this->uri = $uri;
+		
 	}
-
+	
 }
 
 ?>
