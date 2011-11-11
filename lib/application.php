79bb8957e19f5ca65d38098ea0529539cd7bdd6a
David Haywood Smith <davehs@gmail.com>
2011-11-11 22:37:32 +0000

    Fixing notice errors in lib/application.php, take 2

--- lib/application.php
+++ lib/application.php
@@ -96,20 +96,34 @@ class Application {
 		$dot_split = preg_split("/\./", $request);
 		
 		// Grab format from after '.' but before '?'
-		$format_split = preg_split("/\?/", $dot_split[1]);
-		$format = $format_split[0];
+		if (isset($dot_split[1])) {
+			$format_split = preg_split("/\?/", $dot_split[1]);
+			$format = $format_split[0];
+		} else {
+			$format = NULL;
+		}
 		
 		// Split request at each '/' to obtain route (using everything before '.')
 		$segments = preg_split("/\//", $dot_split[0]);
 		
 		// Set up uri variable to pass to app
-		$uri = array(	'controller'	=> $segments[1],
-						'action'		=> $segments[2],
-						'format'		=> $format,
-						'params'		=> array_map('htmlentities', $_GET)
-					);
+
+		$uri['controller'] = $segments[1];
 		
-		$uri['params']['id'] = $segments[3];
+		if (isset($segments[2])) {
+			$uri['action'] = $segments[2];
+		} else {
+			$uri['action'] = NULL;
+		}
+		
+		$uri['format'] = $format;
+		$uri['params'] = array_map('htmlentities', $_GET);
+		
+		if (isset($segments[3])) {
+			$uri['params']['id'] = $segments[3];
+		} else {
+			$uri['params']['id'] = NULL;
+		}
 		
 		// Set the controller to the default if not in URI
 		if (empty($uri['controller'])) {
@@ -259,7 +273,11 @@ class Application {
 	private function loadAction() {
 		
 		if (method_exists($this, $this->uri['action'])) {
-			$this->{$this->uri['action']}($this->uri['params']['id']);
+			if (isset($this->uri['params']['id'])) {
+				$this->{$this->uri['action']}($this->uri['params']['id']);
+			} else {
+				$this->{$this->uri['action']}();
+			}
 		} elseif (empty($this->uri['action']) && method_exists($this, 'index')) {
 			$this->index($this->uri['params']['id']);
 		} else {
