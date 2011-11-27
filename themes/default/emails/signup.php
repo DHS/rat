372f89dd74e28e96073b7788f135afef31448cd3
David Haywood Smith <davehs@gmail.com>
2011-11-27 20:16:23 +0000

    Tweaked signup email

--- themes/default/emails/signup.php
+++ themes/default/emails/signup.php
@@ -3,10 +3,8 @@
 $subject = '['.$this->config->name.'] Welcome to '.$this->config->name.'!';
 
 $body = '<p>Hi '.$_POST['username'].',</p>
-<p>Thank you for joining '.$this->config->name.'!</p>
-<p>We\'ve got some great content - I hope you find something that takes your interest.</p>
-<p>Your feedback is very valuable to me so do reply to this email with your thoughts so far.</p>
-<p>Thanks again,</p>
+<p>Thank you for joining '.$this->config->name.'! Your feedback is very valuable to us so once you\'ve tried our service out please hit reply and let us know your thoughts so far.</p>
+<p>Thanks in advance,</p>
 <p>David Haywood Smith, creator of '.$this->config->name.'</p>';
 
 ?>
