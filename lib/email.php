5d002be64f406a51de9203dfbb2e1308961b7e82
David Haywood Smith <davehs@gmail.com>
2012-01-31 15:11:57 +0000

    Improvements to email lib

--- lib/email.php
+++ lib/email.php
@@ -1,12 +1,30 @@
 <?php
 
 // Helper function to convert raw input to html
-function send_email($to, $subject, $body) {
+function send_email($to, $subject, $body, $debug = FALSE) {
 	
-	$to			= "{$to['name']} <{$to['email']}>";
-	$headers	= "From: {$user->username} <{$user->email}>\r\nContent-type: text/html\r\n";
+	global $config;
+	echo $config->config->name;
 	
-	mail($to, $subject, $body, $headers);
+	if ($app->config->send_emails == TRUE) {
+		
+		$admin = User::get_by_id($app->config->admin_users[0]);
+		
+		if ($to['name']) {
+			$to = "{$to['name']} <{$to['email']}>";
+		} elseif ($to['email']) {
+			$to = $to['email'];
+		}
+		
+		$headers = "From: {$app->config->send_emails_from}\r\nBcc: {$admin->email}\r\nContent-type: text/html\r\n";
+		
+		if ($debug == TRUE) {
+			echo "$to<br />$subject<br />$body<br />$headers";
+		} else {
+			mail($to, $subject, $body, $headers);
+		}
+		
+	}
 	
 }
 
