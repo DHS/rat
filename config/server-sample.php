9fc1cc808d84f32b1b1a959c43a0e7cb7bc6d070
David Haywood Smith <davehs@gmail.com>
2012-03-29 21:40:59 +0100

    Indenting in sample server config

--- config/server-sample.php
+++ config/server-sample.php
@@ -3,36 +3,38 @@
 class ServerConfig {
 
 	// URLs - must include http:// and no trailing slash
-	public $url								= 'http://example.com';
-	public $dev_url							= 'http://localhost';
+	public $url     = 'http://example.com';
+	public $dev_url = 'http://localhost';
 
 	// Base directory - the directory in which your site resides if not in the server root
-	public $base_dir						= '/';
-	public $dev_base_dir					= '/';
+	public $base_dir      = '/';
+	public $dev_base_dir  = '/';
 
 	// Email enabled - search project for "// Email user" to find what this affects
-	public $send_emails						= FALSE;
+	public $send_emails = FALSE;
 
 	// Encryption salt - change to a random six character string, do not change after first use of application
-	public $encryption_salt					= 'hw9e46';
+	public $encryption_salt = 'hw9e46';
 
 	// Set timezone
-	public $timezone						= 'Europe/London';
+	public $timezone = 'Europe/London';
 
 	// Database
-	public $database = array(	'dev'	=> array(	'host'		=> 'localhost',
-													'username'	=> 'root',
-													'password'	=> 'root',
-													'database'	=> 'rat',
-													'prefix'	=> ''
-												),
-
-								'live'	=> array(	'host'		=> 'localhost',
-													'username'	=> 'root',
-													'password'	=> 'root',
-													'database'	=> 'rat',
-													'prefix'	=> ''
-												)
-							);
+	public $database = array(
+	  'dev'	=> array(
+	    'host'      => 'localhost',
+      'username'  => 'root',
+      'password'	=> 'root',
+      'database'	=> 'rat',
+      'prefix'	=> ''
+		),
+    'live'	=> array(
+        'host'		=> 'localhost',
+        'username'	=> 'root',
+        'password'	=> 'root',
+        'database'	=> 'rat',
+        'prefix'	=> ''
+    )
+  );
 
 }
