f4eef7a6c46cc50dfdbe7889bb70d2d127622f0d
David Haywood Smith <davehs@gmail.com>
2012-03-29 21:52:10 +0100

    Line length in mysql lib

--- lib/mysql.php
+++ lib/mysql.php
@@ -2,10 +2,16 @@
 
 // Create database connection
 
-$connection = mysqli_connect($this->config->database[SITE_IDENTIFIER]['host'], $this->config->database[SITE_IDENTIFIER]['username'], $this->config->database[SITE_IDENTIFIER]['password'], $this->config->database[SITE_IDENTIFIER]['database']);
+$connection = mysqli_connect(
+  $this->config->database[SITE_IDENTIFIER]['host'],
+  $this->config->database[SITE_IDENTIFIER]['username'],
+  $this->config->database[SITE_IDENTIFIER]['password'],
+  $this->config->database[SITE_IDENTIFIER]['database']
+);
 
-if ($connection == false) { throw new ApplicationException($this, "Couldn't connect to server."); }
-
+if ($connection == false) {
+  throw new ApplicationException($this, "Couldn't connect to server.");
+}
 
 // SQL injection protection function
 
