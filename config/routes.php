14c405548c45822d489d6dc1f75ce58346540338
David Haywood Smith <davehs@gmail.com>
2012-05-24 19:07:46 +0100

    Fix indenting in routes

--- config/routes.php
+++ config/routes.php
@@ -2,38 +2,38 @@
 
 class Routes {
 
-    public $aliases = array(
+  public $aliases = array(
 
-        // Static pages
+    // Static pages
 
-        '/' => array('controller' => 'items'),
-        '/about' => array('controller' => 'pages', 'action' => 'show', 'id' => 'about'),
-        '/contact' => array('controller' => 'pages', 'action' => 'show', 'id' => 'contact'),
-        '/help' => array('controller' => 'pages', 'action' => 'show', 'id' => 'help'),
-        '/api' => array('controller' => 'pages', 'action' => 'show', 'id' => 'api'),
+    '/' => array('controller' => 'items'),
+    '/about' => array('controller' => 'pages', 'action' => 'show', 'id' => 'about'),
+    '/contact' => array('controller' => 'pages', 'action' => 'show', 'id' => 'contact'),
+    '/help' => array('controller' => 'pages', 'action' => 'show', 'id' => 'help'),
+    '/api' => array('controller' => 'pages', 'action' => 'show', 'id' => 'api'),
 
-        // User functions
+    // User functions
 
-        '/signup' => array('controller' => 'users', 'action' => 'add'),
-        '/login' => array('controller' => 'sessions', 'action' => 'add'),
-        '/logout' => array('controller' => 'sessions', 'action' => 'remove'),
-        '/settings' => array('controller' => 'users', 'action' => 'update'),
-        '/settings/profile' => array('controller' => 'users', 'action' => 'update', 'id' => 'profile'),
-        '/settings/password' => array('controller' => 'users', 'action' => 'update', 'id' => 'password'),
-        '/feed' => array('controller' => 'items', 'action' => 'feed'),
+    '/signup' => array('controller' => 'users', 'action' => 'add'),
+    '/login' => array('controller' => 'sessions', 'action' => 'add'),
+    '/logout' => array('controller' => 'sessions', 'action' => 'remove'),
+    '/settings' => array('controller' => 'users', 'action' => 'update'),
+    '/settings/profile' => array('controller' => 'users', 'action' => 'update', 'id' => 'profile'),
+    '/settings/password' => array('controller' => 'users', 'action' => 'update', 'id' => 'password'),
+    '/feed' => array('controller' => 'items', 'action' => 'feed'),
 
-        // More complex
-        // Complex routes are bottom-up prioritised, i.e. the lowest route has the highest precedence
+    // More complex
+    // Complex routes are bottom-up prioritised, i.e. the lowest route has the highest precedence
 
-        '/signup/*' => array('controller' => 'users', 'action' => 'add', 'code' => '$1'),
+    '/signup/*' => array('controller' => 'users', 'action' => 'add', 'code' => '$1'),
 
-        // eg. /username
-        '/*' => array('controller' => 'users', 'action' => 'show', 'id' => '$1'),
-        // eg. /username/page
-        '/*/*' => array('controller' => 'users', 'action' => 'show', 'id' => '$1', 'page' => '$2'),
-        // eg. /username/item/item_id
-        '/*/item/*' => array('controller' => 'items', 'action' => 'show', 'username' => '$1', 'id' => '$2')
+    // eg. /username
+    '/*' => array('controller' => 'users', 'action' => 'show', 'id' => '$1'),
+    // eg. /username/page
+    '/*/*' => array('controller' => 'users', 'action' => 'show', 'id' => '$1', 'page' => '$2'),
+    // eg. /username/item/item_id
+    '/*/item/*' => array('controller' => 'items', 'action' => 'show', 'username' => '$1', 'id' => '$2')
 
-    );
+  );
 
 }
