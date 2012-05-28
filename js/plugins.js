bb3ff3dbd60bb24650d1fc2d07a5e3e41065c381
David Haywood Smith <davehs@gmail.com>
2012-05-29 00:31:04 +0100

    Content fields use TinyMCE

--- js/plugins.js
+++ js/plugins.js
@@ -4,3 +4,16 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 
 // Pretty dates
 $('span').humaneDates({'lowercase': true});
+
+// TinyMCE
+setTimeout ( function() {
+  tinyMCE.init({
+    mode: "textareas",
+    theme: "advanced",
+    theme_advanced_buttons1: "bold,italic,underline,|,bullist,numlist,|,blockquote,|,link,unlink,|,code",
+    theme_advanced_buttons2: "",
+    theme_advanced_buttons3: "",
+    theme_advanced_toolbar_location: "top",
+    theme_advanced_resizing: true
+  });
+}, 500);
