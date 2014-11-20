1481afc72dc18e205d5e3c3f1549776c7142553e
David Haywood Smith <davehs@gmail.com>
2014-11-20 01:26:07 +0000

    Limit ludicrous line length

--- controllers/items_controller.php
+++ controllers/items_controller.php
@@ -48,7 +48,19 @@ class ItemsController extends Application {
   // Add an item
   function add() {
 
-    if ((isset($_POST['content']) && $_POST['content'] != '') || ($this->config->items['titles']['enabled'] == TRUE && isset($_POST['title']) && $_POST['title'] != '') || ($this->config->items['uploads']['enabled'] == TRUE && isset($_FILES['file']['name']) && $_FILES['file']['name'] != '')) {
+    if (
+      (isset($_POST['content'])
+        && $_POST['content'] != '')
+      || (
+        $this->config->items->titles->enabled == TRUE
+        && isset($_POST['title'])
+        && $_POST['title'] != ''
+      )
+      || (
+        $this->config->items->uploads->enabled == TRUE
+        && isset($_FILES['file']['name'])
+        && $_FILES['file']['name'] != '')
+      ) {
 
       $error = '';
 
