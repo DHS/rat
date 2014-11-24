14585b71010b31d80f4479c52a9f226e79b6bf71
David Haywood Smith <davehs@gmail.com>
2014-11-24 07:26:28 +0000

    Remove hardcoded config vars

--- models/config.php
+++ models/config.php
@@ -4,132 +4,15 @@ class Config {
 
   private $_config_path = 'config/config.json';
 
-  // URLs - must include http:// and no trailing slash
-  public $url     = 'http://example.com';
-  public $dev_url = 'http://127.0.0.1';
-
-  // Base directory - the directory in which your site resides if not in the server root
-  public $live_base_dir      = '/';
-  public $dev_base_dir  = '/rat';
-
-  // Email enabled - search project for "// Email user" to find what this affects
-  public $send_emails = TRUE;
-
-  // Encryption salt - change to a random six character string, do not change after first use of application
-  public $encryption_salt = 'hw9e46';
-
-  // Set timezone
-  public $timezone = 'Europe/London';
-
-  // Database
-  public $database = array(
-    'dev' => array(
-      'host'      => 'localhost',
-      'username'  => 'root',
-      'password'  => '',
-      'database'  => 'rat',
-      'prefix'    => ''
-    ),
-    'live' => array(
-      'host'      => 'localhost',
-      'username'  => 'root',
-      'password'  => 'root',
-      'database'  => 'rat',
-      'prefix'    => ''
-    )
-  );
-
-  // Basic app variables
-  public $name                = 'Ratter';
-  public $tagline             = 'Ratter is an app to demonstrate the functionality of <a href="http://github.com/DHS/rat">Rat</a>';
-  public $default_controller  = 'items';
-
-  // Beta - users can't signup, can only enter their email addresses
-  public $beta = TRUE;
-
-  // Private app - requires login to view pages (except public_pages)
-  // no share buttons
-  public $private = FALSE;
-  public $signup_email_notifications = TRUE;
-
-  // Items
-  // Notes about uploads: max-size is in bytes (default: 5MB), directory
-  // should contain three subdirectories: originals, thumbnails, stream
-  public $items = array(
-    'name'        => 'post',
-    'name_plural' => 'posts',
-
-    'titles' => array(
-      'enabled'     => TRUE,
-      'name'        => 'Title',
-      'name_plural' => 'Titles'
-    ),
-
-    'content' => array(
-      'enabled'     => TRUE,
-      'name'        => 'Content',
-      'name_plural' => 'Contents'
-    ),
-
-    // Remember to update the permissions for your
-    // upload dir e.g. chmod -R 777 uploads
-    'uploads'       => array(
-      'enabled'     => TRUE,
-      'name'        => 'Image',
-      'directory'   => 'uploads',
-      'max-size'    => '5242880',
-      'mime-types'  => 'image/jpeg,image/png,image/gif,image/pjpeg'
-    ),
-
-    'comments' => array(
-      'enabled'     => TRUE,
-      'name'        => 'Comment',
-      'name_plural' => 'Comments'
-    ),
-
-    'likes' => array(
-      'enabled'       => TRUE,
-      'name'          => 'Like',
-      'name_plural'   => 'Likes',
-      'opposite_name' => 'Unlike',
-      'past_tense'    => 'Liked by'
-    )
-
-  );
-
-  // Invites system
-  public $invites = array('enabled' => TRUE);
-
-  // Friends - still testing, works with asymmetric set to true... just!
-  // (Shows 'Follow' link & generates homepage feed)
-  public $friends = array(
-    'enabled'     => FALSE,
-    'asymmetric'  => FALSE
-  );
-
-  // Admin users - array of user IDs who have access to admin area
-  public $admin_users = '1';
-
-  // Theme
-  public $theme = 'default';
-
-  // Plugins
-  public $plugins = array(
-    'log'       => TRUE,
-    'gravatar'  => TRUE,
-    'points'    => FALSE,
-    'analytics' => FALSE
-  );
-
-  // Send emails from what address?
-  public $send_emails_from = 'support@blah.com';
-
-  public $site_identifier;
-  public $base_dir;
-
   public function __construct() {
+
+    // Fetch config from config.json
     $raw_config = $this->loadConfigFile();
+
+    // overwrite declared vars with those loaded from config.json
     self::fillObject($this, $raw_config);
+
+    // Process config to setup base_dir and url
     $this->processConfig();
   }
 
